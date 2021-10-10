c3b0540855e1b94a3f80eec82cefc41d6a0ef1be
sadhippo <accooper64@gmail.com>
2021-10-10 13:17:34 -0400

    Delete CGContractTest directory

--- CGContractTest/migrations/2_cryptoguns.js
+++
@@ -1,8 +0,0 @@
-const { deployProxy } = require('@openzeppelin/truffle-upgrades');
-const Cryptoguns = artifacts.require("Cryptoguns");
-
-module.exports = async function (deployer) {
-  const game =  await deployProxy(Cryptoguns,{ deployer });
-  console.log('Deployed', game.address);
-
-};
