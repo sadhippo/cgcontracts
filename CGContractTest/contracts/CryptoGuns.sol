6f3e6e555b52a0e58d4aa6777ffab438f28a448f
sadhippo <accooper64@gmail.com>
2021-12-14 16:06:16 -0500

    ChainLinkVRF_Update
    Added in chainlinks VRF Generator
    removed approve from transfer
    added with withdraw for link and acceptToken
    removed Rarity variable from Swat struct

--- CGContractTest/contracts/CryptoGuns.sol
+++ CGContractTest/contracts/CryptoGuns.sol
@@ -8,16 +8,15 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721BurnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
-//import "./VRFConsumerBaseUpgradeable.sol";
+import "./VRFConsumerBaseUpgradable.sol";
 
 
-contract CryptoGuns is AccessControlUpgradeable, ERC721URIStorageUpgradeable{
+contract CryptoGuns is AccessControlUpgradeable, ERC721URIStorageUpgradeable, VRFConsumerBaseUpgradable{
   using SafeMath32 for uint32;
   using SafeMathUpgradeable for uint256;
 
   struct Swat {
     string name;
-    string rarity;
     bool upgraded;
     uint32 readyTime;
   }
@@ -26,9 +25,10 @@ contract CryptoGuns is AccessControlUpgradeable, ERC721URIStorageUpgradeable{
 event NewSwat(address indexed _player, uint _swatId, string _name);
 event Burn(address indexed _player, uint256 _swatId);
 event SwatPriceSet(uint newPrice);
-event NewSpecificSwat(address indexed _receivingPlayer, string _name, string _rarity);
+event NewSpecificSwat(address indexed _receivingPlayer, string _name);
 event Purchased(address indexed _receivingPlayer);
 event TokenWithdrawal(uint _amount);
+event LinkTokenWithdrawal(uint _amount);
 
 
 //Mappings
@@ -36,8 +36,8 @@ mapping(address => uint[]) public userOwnedSwats; //tokenID of swat units in wal
 mapping(uint => uint) public swatIsAtIndex; // index of the tokenID in the wallet
 mapping (address => uint) public ownerSwatCount; //# of swat units in wallet
 //VRF MAPPINGS
-//mapping(bytes32 => address) requestToSender;
-//mapping(bytes32 => uint256) requestToTokenId;
+mapping(bytes32 => address) requestToSender;
+mapping(bytes32 => uint256) requestToTokenId;
 
 // Access Control Roles
 bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
@@ -46,9 +46,9 @@ bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 Swat[] public swats;
 
 //VRF VARIABLES
-//bytes32 internal keyHash;
-//uint256 internal fee;
-//uint256 public roll;
+bytes32 internal keyHash;
+uint256 internal fee;
+uint256 public roll;
 //address public VRFCoordinator;
 // bsc testnet: 0xa555fC018435bef5A13C6c6870a9d4C11DEC329C
 //address public LinkToken;
@@ -73,22 +73,22 @@ function initialize() public initializer {
      __ERC721_init("CryptoGuns Squad Members", "SquadMembers");
      __AccessControl_init();
      __ERC721URIStorage_init();
-     // __VRFConsumerBase_init(_VRFCoordinator, _LinkToken);
      _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
      _setupRole(MINTER_ROLE, _msgSender());
+     VRFConsumerBaseUpgradable.initialize(
+            0xa555fC018435bef5A13C6c6870a9d4C11DEC329C, // VRF Coordinator
+            0x84b9B910527Ad5C03A9Ca831909E21e236EA7b06  // LINK Token
+        );
          // for mainnet address bananaAdress = 0x603c7f932ed1fc6575303d8fb018fdcbb0f39a95;
          // we made a test simple bep20token for testnet. - 0x0C69F8B5133038D445d9dc9CA53a0061FE260Ea6
      _acceptedToken = 0x603c7f932ED1fc6575303D8Fb018fDCBb0f39a95;
-
+     keyHash = 0xcaf3c3727e033261d383b315559476f48034c13b18f8cafed4d871abe5049186;
       cooldownTime = 1 days;
       nonce = 0;
       acceptedToken = ERC20Interface(_acceptedToken);
-
-      // VRFCoordinator = _VRFCoordinator;
-      //  LinkToken = _LinkToken;
-      //  keyHash = _keyhash;
-      // fee = 0.1 * 10**18; // 0.1 LINK
+      fee = 0.1 * 10**18; // 0.1 LINK
 }
+
   function _baseURI() internal pure override returns (string memory) {
     return "https://www.cryptoguns.io/json/";
   }
@@ -104,31 +104,36 @@ function addAdmin(address account) public virtual {
 
 
  //VRF FUNCTIONS
- //**
+ // **
  //    * Requests randomness
  //    */
- //   function getRandomNumber() public returns (bytes32 requestId) {
- //       require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK - fill contract with faucet");
- //       requestId = requestRandomness(keyHash, fee);
- //       requestToSender[requestId] = msg.sender;
- //       return requestRandomness(keyHash, fee);
- //   }
+   function getRandomNumber() public returns (bytes32 requestId) {
+       require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK - fill contract with faucet");
+       requestId = requestRandomness(keyHash, fee);
+       requestToSender[requestId] = msg.sender;
+    //   return requestRandomness(keyHash, fee);
+   }
 
 
 
-   // function withdrawLink() external {} - Implement a withdraw function to avoid locking your LINK in the contract
+    function withdrawLink(uint _amount) external { //Implement a withdraw function to avoid locking your LINK in the contract
+      require(hasRole(DEFAULT_ADMIN_ROLE, _msgSender()));
+      require(LINK.transfer(_msgSender(), _amount), "Transferring the desire amount failed");
+      emit LinkTokenWithdrawal(_amount);
 
-//assigns URI to swat based on name & rarity. Unity Engine reads the URI to pull character attributes for in-game stats.
-  function _mintSwat(string memory _name, string memory _rarity, address _owner) internal {
+    }
 
-    swats.push(Swat(_name, _rarity, false, uint32(block.timestamp + cooldownTime)));
+//assigns URI to swat based on name. Unity Engine reads the URI to pull character attributes for in-game stats.
+  function _mintSwat(string memory _name,address _owner) internal {
+
+    swats.push(Swat(_name, false, uint32(block.timestamp + cooldownTime)));
     uint id = swats.length - 1;
     ownerSwatCount[msg.sender] = ownerSwatCount[msg.sender] + 1;
     userOwnedSwats[msg.sender].push(id);
     uint ownedSwatLength = userOwnedSwats[_msgSender()].length;
     swatIsAtIndex[id] = ownedSwatLength -1;
     _safeMint(_owner, id);
-    emit NewSwat( _owner, id, _name);
+    emit NewSwat( msg.sender, id, _name);
     bytes32 swatName = keccak256(abi.encodePacked(_name));
     if(swatName == keccak256("jason")){
       _setTokenURI(id, _name);
@@ -192,93 +197,94 @@ function addAdmin(address account) public virtual {
      }
     }
 
-  function _randomRoll() internal returns (uint) {
-    uint randomnumber = uint(keccak256(abi.encodePacked(block.timestamp, _msgSender(), nonce))) % 1000;
-    nonce++;
-    return randomnumber;
-  }
+  // function _randomRoll() internal returns (uint) {
+  //   uint randomnumber = uint(keccak256(abi.encodePacked(block.timestamp, _msgSender(), nonce))) % 1000;
+  //   nonce++;
+  //   return randomnumber;
+  // }
 //
 // VRF Function
 // function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
-//     roll = (randomness % 1000) + 1;
-  function mintRandomSwat(address _owner) internal {
+//
+  //function mintRandomSwat(address _owner) public {
+  function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
     string memory randName;
-    string memory randRarity;
- require(_msgSender() == _owner, "A player can only mint to their own wallet.");
+     address requestAddress = requestToSender[requestId];
     // .2% for mythic, .7% for legendary, 10% for rare, 35% uncommon, rest Common
-    uint roll = _randomRoll();
+    roll = (randomness % 1000) + 1;
+
     if(roll >= 998) {
       randName = "jasonlake";
-      randRarity = "mythic";}
+    }
     else if(roll >= 996)
       {randName = "discipliner";
-      randRarity = "mythic";}
+      }
     else if(roll > 994)
       {randName = "nightmare";
-      randRarity = "mythic";}
+      }
 
     else if(roll > 991){
       randName = "sarge";
-      randRarity = "legendary";}
+    }
     else if(roll > 987){
       randName = "mastersee";
-      randRarity = "legendary";}
+    }
     else if(roll > 984){
       randName = "loki";
-      randRarity = "legendary";}
+  }
 
     else if(roll > 950){
       randName = "jaguar";
-      randRarity = "rare";}
+    }
     else if(roll > 930){
       randName = "pistolpete";
-      randRarity = "rare";}
+    }
     else if(roll > 910){
       randName = "freya";
-      randRarity = "rare";}
+    }
     else if(roll > 890){
       randName = "eagleeye";
-      randRarity = "rare";}
+    }
     else if(roll > 870){
       randName = "rumple";
-      randRarity = "rare";}
+      }
     else if(roll > 850){
       randName = "terrorrick";
-      randRarity = "rare"; }
+    }
 
     else if(roll > 750)
       {randName = "pump";
-      randRarity = "Uncommon"; }
+    }
     else if(roll > 650)
       {randName = "action";
-      randRarity = "Uncommon"; }
+      }
     else if(roll > 500){
       randName = "jimrimbo";
-      randRarity = "Uncommon"; }
+      }
 
     else if(roll > 400){
       randName = "keithurban";
-      randRarity = "Common"; }
+    }
     else if(roll > 300){
       randName = "steve";
-      randRarity = "Common"; }
+     }
     else if(roll > 200){
       randName = "woodsie";
-      randRarity = "Common"; }
+     }
     else if(roll > 100){
       randName = "phil";
-      randRarity = "Common"; }
+   }
     else {
       randName = "basicbob";
-      randRarity = "Common"; }
-  _mintSwat(randName, randRarity, _owner);
+       }
+  _mintSwat(randName, requestAddress);
 }
 
-//for promotions/giveaways/in-game rewards
-function mintSpecificSwat(address _owner, string memory _name, string memory _rarity) public{
-  require(hasRole(MINTER_ROLE, _msgSender()), "User does not have Admin Role");
- _mintSwat(_name, _rarity, _owner);
- emit NewSpecificSwat(_owner, _name, _rarity);
+
+function mintSpecificSwat(address _owner, string memory _name) public{
+  require(hasRole(MINTER_ROLE, _msgSender()), "User does not have Minter Role");
+ _mintSwat(_name, _owner);
+ emit NewSpecificSwat(_owner, _name);
 }
 
 
@@ -340,10 +346,6 @@ function duplicateUpgrade(uint _swatId, uint _targetId) public{
   }
 
 
-  function getRarity(uint _swatId) public view returns (string memory) {
-    return swats[_swatId].rarity;
-  }
-
   function getReadyTime(uint _swatId) public view returns (uint) {
     return swats[_swatId].readyTime;
   }
@@ -373,19 +375,19 @@ function getSwatIndex(uint _id) public view returns (uint){
 
 //  BuyNFT Functions
 // Use Approve function on token on front-end UI before running this.
-  function buyNFT(address _buyer) public {
-    require(_msgSender() == _buyer, "A player can only buy characters for their own wallet");
-    require(acceptedToken.transferFrom(_msgSender(), address(this), swatPrice), "Transferring the sale amount failed");
-    mintRandomSwat(_buyer);
-
-    emit Purchased(_buyer);
-      }
+  // function buyNFT(address _buyer) public {
+  //   require(_msgSender() == _buyer, "A player can only buy characters for their own wallet");
+  //   require(acceptedToken.transferFrom(_msgSender(), address(this), swatPrice), "Transferring the sale amount failed");
+  //   mintRandomSwat(_buyer);
+  //
+  //   emit Purchased(_buyer);
+  //     }
 
 
      function setSwatPrice(uint256 _price) public {
         require(hasRole(DEFAULT_ADMIN_ROLE, _msgSender()), "User does not have Admin Role");
-       require(_price > 1 * 10^18, "Price can not less than 1");
-       require(_price != 20 * 10^18, "Price can not be more than 20s");
+       require(_price > 1 * 10**18, "Price can not less than 1");
+       require(_price != 20 * 10**18, "Price can not be more than 20s");
        swatPrice = _price;
 
        emit SwatPriceSet(_price);
@@ -398,7 +400,7 @@ function getSwatIndex(uint _id) public view returns (uint){
 
      function withdrawToken (uint _amount) external{
      require(hasRole(DEFAULT_ADMIN_ROLE, _msgSender()), "Must have admin role to perform this operation");
-     require(acceptedToken.transfer(address(this), _msgSender(), _amount), "Transferring the desire amount failed");
+     require(acceptedToken.transfer(_msgSender(), _amount), "Transferring the desire amount failed");
      emit TokenWithdrawal(_amount);
      }
 }
